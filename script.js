b250bac816b3141d49cba76b6c59fab5623998ac
Hitesh Marwaha <73687191+hitu69@users.noreply.github.com>
2023-04-12 11:57:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+// const p = document.createElement("p");
+const log = document.querySelector("body");
+document.addEventListener("DOMContentLoaded", (event) => {
+log.innerText = "DOM load success";
+});
+//p.innerText="DOM load success";
+// const node = document.createTextNode("DOM load success");
+// p.appendChild(node);
+document.body.appendChild(p)
